e0e49ae377ea4a355584962f4a1b1884067f4b73
Egor Lokarev <yogorus@gmail.com>
2024-10-01 17:30:49 +0300

    add djoser and jwt auth

--- workmate/apps/kitten_api/apps.py
+++ workmate/apps/kitten_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class KittenApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kitten_api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.kitten_api"
--- workmate/apps/kitten_api/models.py
+++ workmate/apps/kitten_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class Breed(models.Model):
+#     pass
+
+
+# class Kitten(models.Model):
+#     name = models.CharField(verbose_name="Kitten name", max_length=50)
+#     color = models.CharField(max_length=50)
+#     owner = models.ForeignKey()
+#     breed = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
