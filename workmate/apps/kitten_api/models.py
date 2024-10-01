ae2476dc57ac2d8ab3fb308fc49efb797cdb5360
Egor Lokarev <yogorus@gmail.com>
2024-10-01 17:46:40 +0300

    add models

--- workmate/apps/kitten_api/models.py
+++ workmate/apps/kitten_api/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 
 
-# class Breed(models.Model):
-#     pass
+class Breed(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    description = models.TextField(blank=True, null=True)
 
 
-# class Kitten(models.Model):
-#     name = models.CharField(verbose_name="Kitten name", max_length=50)
-#     color = models.CharField(max_length=50)
-#     owner = models.ForeignKey()
-#     breed = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+class Kitten(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    color = models.CharField(max_length=50, blank=False)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    breed = models.ForeignKey("kitten_api.Breed", on_delete=models.CASCADE)
